6bc22987fe243d7911132e6f071921dd74984927
Fahmi <fahmiardiyannto@yahoo.com>
2024-02-07 12:10:08 +0700

    build(release): Upload project

---
+++ Xisflix/XisflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  XisflixApp.swift
+//  Xisflix
+//
+//  Created by Muhammad Fahmi on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct XisflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
